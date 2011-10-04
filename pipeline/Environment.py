95410f100dad1a232d421e6ac0cf1b452d0db7b7
Justin Walgran <jwalgran@azavea.com>
2011-10-04 11:39:06 -0400

    Updated Evironment constructor to accept an array of paths
    I need the Environment to be configurable so that a project can define where
    to search for files to include.

--- pipeline/Environment.py
+++ pipeline/Environment.py
@@ -2,9 +2,13 @@ import os
 
 class Environment:
 
-    def __init__(self):
-        self._paths = [os.getcwd()]
-
+    def __init__(self, *args, **kwargs):
+        self._paths = []
+        for arg in args:
+            self._paths.append(arg)
+        if 'include_cwd' not in kwargs or ('include_cwd' in kwargs and kwargs['include_cwd']):
+            self._paths.append(os.getcwd())
+            
     @property
     def paths(self):
         """
--- test/TestEnvironment.py
+++ test/TestEnvironment.py
@@ -12,3 +12,11 @@ class TestEnvironment(unittest.TestCase):
     def test_default_path_property_value_is_an_array_containing_cwd(self):
         env = Environment()
         self.assertEqual([os.getcwd()], env.paths)
+
+    def test_environment_can_be_created_with_paths(self):
+        env = Environment('/tmp/test', '/tmp/lib')
+        self.assertEqual(['/tmp/test', '/tmp/lib', os.getcwd()], env.paths)
+
+    def test_cwd_can_be_omitted_from_paths(self):
+        env = Environment('/tmp/test', '/tmp/lib', include_cwd = False)
+        self.assertEqual(['/tmp/test', '/tmp/lib'], env.paths)
