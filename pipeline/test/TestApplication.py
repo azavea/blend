4b1c96ff869bbe907a3f6e1b961f92c7af374de8
Justin Walgran <jwalgran@azavea.com>
2012-02-10 11:18:25 -0500

    Modify test helper imports so that nosetests runner does not try to run them

--- pipeline/test/TestApplication.py
+++ pipeline/test/TestApplication.py
@@ -1,7 +1,9 @@
 import unittest
 import tempfile
 
-from helpers import *
+import helpers
+import shutil
+import os
 from pipeline import Application
 
 class TestApplication(unittest.TestCase):
@@ -11,7 +13,7 @@ class TestApplication(unittest.TestCase):
 
     def tearDown(self):
         shutil.rmtree(self.test_env_dir)
-        clean_output()
+        helpers.clean_output()
 
     def test_default_env_is_cwd(self):
         app = Application()
@@ -36,22 +38,22 @@ class TestApplication(unittest.TestCase):
     def test_run_with_single_resource(self):
         paths_to_processable_test_files = [
             os.path.join(self.test_env_dir, 'dir1', 'file1.js')]
-        create_test_files(paths_to_processable_test_files)
+        helpers.create_test_files(paths_to_processable_test_files)
         app = Application(file_list=[paths_to_processable_test_files[0]])
         self.assertEqual(0, app.run())
-        clean_up_test_files(paths_to_processable_test_files)
+        helpers.clean_up_test_files(paths_to_processable_test_files)
 
     def test_run_with_single_resource_with_requirement(self):
         paths_to_test_files = [
             os.path.join(self.test_env_dir, 'dir1', 'file1.js'),
             os.path.join(self.test_env_dir, 'dir2', 'file2.js')]
-        clean_up_test_files(paths_to_test_files)
-        create_test_files(paths_to_test_files)
-        create_test_file_with_content(paths_to_test_files[0], '// This is file 1\n//= require <file2>')
-        create_test_file_with_content(paths_to_test_files[1], '// This is file 2')
+        helpers.clean_up_test_files(paths_to_test_files)
+        helpers.create_test_files(paths_to_test_files)
+        helpers.create_test_file_with_content(paths_to_test_files[0], '// This is file 1\n//= require <file2>')
+        helpers.create_test_file_with_content(paths_to_test_files[1], '// This is file 2')
         app = Application(env_path_list=[self.test_env_dir], file_list=[paths_to_test_files[0]])
         self.assertEqual(0, app.run())
-        clean_up_test_files(paths_to_test_files)
+        helpers.clean_up_test_files(paths_to_test_files)
 
     def test_main_exits_cleanly_when_no_args_are_passed(self):
         app = Application
--- pipeline/test/TestResource.py
+++ pipeline/test/TestResource.py
@@ -3,7 +3,10 @@ import tempfile
 
 from pipeline import Resource, Environment
 from pipeline.Requirement import RequirementNotSatisfiedException
-from helpers import *
+import shutil
+import os
+import helpers
+
 
 class TestResource(unittest.TestCase):
     """Asserts that the properties and methods of the Resource class behave correctly."""
@@ -13,7 +16,7 @@ class TestResource(unittest.TestCase):
 
     def tearDown(self):
         shutil.rmtree(self.test_env_dir)
-        clean_output()
+        helpers.clean_output()
 
     def test_resource_must_be_created_with_path_to_file(self):
         self.assertRaises(Exception, Resource)
@@ -74,18 +77,18 @@ class TestResource(unittest.TestCase):
     def test_find_all_javascript_resources_in_the_default_environment(self):
         paths_to_test_files = [os.path.join(self.test_env_dir, 'test.js'), os.path.join(self.test_env_dir, 'test.css'),
             os.path.join(self.test_env_dir, 'test.html')]
-        create_test_files(paths_to_test_files)
+        helpers.create_test_files(paths_to_test_files)
         resources = Resource.find_all_of_type_in_environment('javascript', Environment(self.test_env_dir))
-        clean_up_test_files(paths_to_test_files)
+        helpers.clean_up_test_files(paths_to_test_files)
         self.assertEquals(1, len(resources), 'One and only one javascript file should be found')
 
     def test_find_all_javascript_resources_in_an_environment(self):
         paths_to_test_files = [os.path.join(self.test_env_dir, 'subdir', 'test.js'),
             os.path.join(self.test_env_dir, 'test.css'), os.path.join(self.test_env_dir, 'test.html')]
         test_env = Environment(self.test_env_dir, include_cwd=False)
-        create_test_files(paths_to_test_files)
+        helpers.create_test_files(paths_to_test_files)
         resources = Resource.find_all_of_type_in_environment('javascript', test_env)
-        clean_up_test_files(paths_to_test_files)
+        helpers.clean_up_test_files(paths_to_test_files)
         self.assertEquals(1, len(resources), 'One and only one javascript file should be found')
         self.assertEqual(os.path.join(self.test_env_dir, 'subdir', 'test.js'),
             resources[0].path_to_file)
@@ -93,27 +96,27 @@ class TestResource(unittest.TestCase):
     def test_search_for_javascript_resources_in_an_environment_without_any_returns_none(self):
         test_env = Environment(self.test_env_dir, include_cwd=False)
         path_to_test_file = os.path.join(self.test_env_dir, 'test.css')
-        create_test_files(path_to_test_file)
+        helpers.create_test_files(path_to_test_file)
         resources = Resource.find_all_of_type_in_environment('javascript', test_env)
         self.assertEquals(None, resources)
 
     def test_exists_property(self):
         path_to_test_file = os.path.join(self.test_env_dir, 'test.js')
-        clean_up_test_files(path_to_test_file)
+        helpers.clean_up_test_files(path_to_test_file)
         resource = Resource(path_to_test_file)
         # file does not exist yet
         self.assertFalse(resource.exists)
-        create_test_files(path_to_test_file)
+        helpers.create_test_files(path_to_test_file)
         self.assertTrue(resource.exists)
-        clean_up_test_files(path_to_test_file)
+        helpers.clean_up_test_files(path_to_test_file)
 
     def test_content_property(self):
         path_to_test_file = os.path.join(self.test_env_dir, 'test.js')
         content = 'var foo = {};'
-        create_test_file_with_content(path_to_test_file, content)
+        helpers.create_test_file_with_content(path_to_test_file, content)
         resource = Resource(path_to_test_file)
         self.assertEquals(content, resource.content)
-        clean_up_test_files(path_to_test_file)
+        helpers.clean_up_test_files(path_to_test_file)
 
     def test_requirements_property_for_resource_without_content_is_none(self):
         resource = Resource('some file')
@@ -122,19 +125,19 @@ class TestResource(unittest.TestCase):
 
     def test_requirements_property_for_resource_with_plain_content_is_none(self):
         path_to_test_file = os.path.join(self.test_env_dir, 'test.js')
-        clean_up_test_files(path_to_test_file)
+        helpers.clean_up_test_files(path_to_test_file)
         content = 'var foo = {};'
-        create_test_file_with_content(path_to_test_file, content)
+        helpers.create_test_file_with_content(path_to_test_file, content)
         resource = Resource(path_to_test_file)
         self.assertTrue(resource.content == content)
         self.assertEqual(None, resource.requirements)
-        clean_up_test_files(path_to_test_file)
+        helpers.clean_up_test_files(path_to_test_file)
 
     def test_javascript_requirements_are_found(self):
         path_to_test_file = os.path.join(self.test_env_dir, 'test.js')
-        clean_up_test_files(path_to_test_file)
+        helpers.clean_up_test_files(path_to_test_file)
         content = '//= require <jquery>\nvar foo = {};//= require "openlayers"\n var s = "some other thing"\n'
-        create_test_file_with_content(path_to_test_file, content)
+        helpers.create_test_file_with_content(path_to_test_file, content)
         resource = Resource(path_to_test_file)
         self.assertTrue(resource.content == content)
         self.assertEqual(2, len(resource.requirements))
@@ -144,13 +147,13 @@ class TestResource(unittest.TestCase):
         self.assertEqual('openlayers', resource.requirements[1].name)
         self.assertEqual('local', resource.requirements[1].type)
         self.assertEqual((34,59), resource.requirements[1].insert_location)
-        clean_up_test_files(path_to_test_file)
+        helpers.clean_up_test_files(path_to_test_file)
 
     def test_css_import_statements_found_as_requirements(self):
         path_to_test_file = os.path.join(self.test_env_dir, 'test.css')
-        clean_up_test_files(path_to_test_file)
+        helpers.clean_up_test_files(path_to_test_file)
         content = 'h1 {background:red;}\n @import url("something.css")'
-        create_test_file_with_content(path_to_test_file, content)
+        helpers.create_test_file_with_content(path_to_test_file, content)
         resource = Resource(path_to_test_file)
         self.assertTrue(resource.content == content)
         self.assertEqual(1, len(resource.requirements))
@@ -160,9 +163,9 @@ class TestResource(unittest.TestCase):
 
     def test_css_require_comments_found_as_requirements(self):
         path_to_test_file = os.path.join(self.test_env_dir, 'test.css')
-        clean_up_test_files(path_to_test_file)
+        helpers.clean_up_test_files(path_to_test_file)
         content = 'h1 {background:red;}\n /*= require "something"  */'
-        create_test_file_with_content(path_to_test_file, content)
+        helpers.create_test_file_with_content(path_to_test_file, content)
         resource = Resource(path_to_test_file)
         self.assertTrue(resource.content == content)
         self.assertEqual(1, len(resource.requirements))
@@ -174,58 +177,58 @@ class TestResource(unittest.TestCase):
         paths_to_test_files = [
             os.path.join(self.test_env_dir, 'dir1', 'file1.js'),
             os.path.join(self.test_env_dir, 'dir2', 'file2.js')]
-        clean_up_test_files(paths_to_test_files)
-        create_test_file_with_content(paths_to_test_files[0], '// This is file 1\n//= require <FILE2>')
-        create_test_file_with_content(paths_to_test_files[1], '// This is file 2')
+        helpers.clean_up_test_files(paths_to_test_files)
+        helpers.create_test_file_with_content(paths_to_test_files[0], '// This is file 1\n//= require <FILE2>')
+        helpers.create_test_file_with_content(paths_to_test_files[1], '// This is file 2')
         file1_resource = Resource(paths_to_test_files[0])
         actual_merged_content = file1_resource.merge_requirements_from_environment(Environment(self.test_env_dir,
             include_cwd=False), previously_merged=[])
 
         self.assertEqual('// This is file 1\n// This is file 2', actual_merged_content)
 
-        clean_up_test_files(paths_to_test_files)
+        helpers.clean_up_test_files(paths_to_test_files)
 
     def test_merge_js_requirements_in_local_path(self):
         paths_to_test_files = [
             os.path.join(self.test_env_dir, 'dir1', 'file1.js'),
             os.path.join(self.test_env_dir, 'dir1', 'file2.js'),
             os.path.join(self.test_env_dir, 'dir2', 'file2.js')]
-        clean_up_test_files(paths_to_test_files)
-        create_test_file_with_content(paths_to_test_files[0], '// This is file 1\n//= require "FILE2"')
-        create_test_file_with_content(paths_to_test_files[1], '// This is LOCAL file 2')
-        create_test_file_with_content(paths_to_test_files[2], '// This is GLOBAL file 2')
+        helpers.clean_up_test_files(paths_to_test_files)
+        helpers.create_test_file_with_content(paths_to_test_files[0], '// This is file 1\n//= require "FILE2"')
+        helpers.create_test_file_with_content(paths_to_test_files[1], '// This is LOCAL file 2')
+        helpers.create_test_file_with_content(paths_to_test_files[2], '// This is GLOBAL file 2')
         file1_resource = Resource(paths_to_test_files[0])
         actual_merged_content = file1_resource.merge_requirements_from_environment(Environment(self.test_env_dir,
                                                                                                include_cwd=False), previously_merged=[])
 
         self.assertEqual('// This is file 1\n// This is LOCAL file 2', actual_merged_content)
 
-        clean_up_test_files(paths_to_test_files)
+        helpers.clean_up_test_files(paths_to_test_files)
 
     def test_merge_css_requirements_in_local_path(self):
         paths_to_test_files = [
             os.path.join(self.test_env_dir, 'dir1', 'file1.css'),
             os.path.join(self.test_env_dir, 'dir1', 'file2.css'),
             os.path.join(self.test_env_dir, 'dir2', 'file2.css')]
-        clean_up_test_files(paths_to_test_files)
-        create_test_file_with_content(paths_to_test_files[0], '/* This is file 1 */\n@import url("FiLe2.css")')
-        create_test_file_with_content(paths_to_test_files[1], '/* This is LOCAL file 2 */')
-        create_test_file_with_content(paths_to_test_files[2], '/* This is GLOBAL file 2 */')
+        helpers.clean_up_test_files(paths_to_test_files)
+        helpers.create_test_file_with_content(paths_to_test_files[0], '/* This is file 1 */\n@import url("FiLe2.css")')
+        helpers.create_test_file_with_content(paths_to_test_files[1], '/* This is LOCAL file 2 */')
+        helpers.create_test_file_with_content(paths_to_test_files[2], '/* This is GLOBAL file 2 */')
         file1_resource = Resource(paths_to_test_files[0])
         actual_merged_content = file1_resource.merge_requirements_from_environment(Environment(self.test_env_dir,
                                                                                                include_cwd=False), previously_merged=[])
 
         self.assertEqual('/* This is file 1 */\n/* This is LOCAL file 2 */', actual_merged_content)
 
-        clean_up_test_files(paths_to_test_files)
+        helpers.clean_up_test_files(paths_to_test_files)
 
     def test_global_resource_not_merged_for_local_requirement(self):
         paths_to_test_files = [
             os.path.join(self.test_env_dir, 'dir1', 'file1.js'),
             os.path.join(self.test_env_dir, 'dir2', 'file2.js')]
-        clean_up_test_files(paths_to_test_files)
-        create_test_file_with_content(paths_to_test_files[0], '// This is file 1\n//= require "FILE2"')
-        create_test_file_with_content(paths_to_test_files[1], '// This is GLOBAL file 2')
+        helpers.clean_up_test_files(paths_to_test_files)
+        helpers.create_test_file_with_content(paths_to_test_files[0], '// This is file 1\n//= require "FILE2"')
+        helpers.create_test_file_with_content(paths_to_test_files[1], '// This is GLOBAL file 2')
         file1_resource = Resource(paths_to_test_files[0])
 
         self.assertRaises(RequirementNotSatisfiedException, file1_resource.merge_requirements_from_environment,
@@ -236,14 +239,14 @@ class TestResource(unittest.TestCase):
                 os.path.join(self.test_env_dir, 'dir1', 'file1.js'),
                 os.path.join(self.test_env_dir, 'dir2', 'file2.js'),
                 os.path.join(self.test_env_dir, 'dir3', 'file3.js')]
-        clean_up_test_files(paths_to_test_files)
-        create_test_file_with_content(paths_to_test_files[0], '// This is file 1\n//= require <FILE2>')
-        create_test_file_with_content(paths_to_test_files[1], '// This is file 2\n//= require <file3>')
-        create_test_file_with_content(paths_to_test_files[2], '// This is file 3')
+        helpers.clean_up_test_files(paths_to_test_files)
+        helpers.create_test_file_with_content(paths_to_test_files[0], '// This is file 1\n//= require <FILE2>')
+        helpers.create_test_file_with_content(paths_to_test_files[1], '// This is file 2\n//= require <file3>')
+        helpers.create_test_file_with_content(paths_to_test_files[2], '// This is file 3')
         file1_resource = Resource(paths_to_test_files[0])
         actual_merged_content = file1_resource.merge_requirements_from_environment(Environment(self.test_env_dir, include_cwd=False), previously_merged=[])
         self.assertEqual('// This is file 1\n// This is file 2\n// This is file 3', actual_merged_content)
-        clean_up_test_files(paths_to_test_files)
+        helpers.clean_up_test_files(paths_to_test_files)
 
     def test_merge_from_global_path_does_not_add_requirements_twice(self):
         paths_to_test_files = [
@@ -251,16 +254,16 @@ class TestResource(unittest.TestCase):
             os.path.join(self.test_env_dir, 'dir21', 'file2.js'),
             os.path.join(self.test_env_dir, 'dir31', 'file3.js'),
             os.path.join(self.test_env_dir, 'dir41', 'file4.js')]
-        clean_up_test_files(paths_to_test_files)
-        create_test_file_with_content(paths_to_test_files[0], '// This is file 1\n//= require <file2>\n//= require <file3>')
-        create_test_file_with_content(paths_to_test_files[1], '// This is file 2\n//= require <file4>')
-        create_test_file_with_content(paths_to_test_files[2], '// This is file 3\n//= require <file4>')
-        create_test_file_with_content(paths_to_test_files[3], '// This is file 4\n')
+        helpers.clean_up_test_files(paths_to_test_files)
+        helpers.create_test_file_with_content(paths_to_test_files[0], '// This is file 1\n//= require <file2>\n//= require <file3>')
+        helpers.create_test_file_with_content(paths_to_test_files[1], '// This is file 2\n//= require <file4>')
+        helpers.create_test_file_with_content(paths_to_test_files[2], '// This is file 3\n//= require <file4>')
+        helpers.create_test_file_with_content(paths_to_test_files[3], '// This is file 4\n')
 
         file1_resource = Resource(paths_to_test_files[0])
         actual_merged_content = file1_resource.merge_requirements_from_environment(Environment(self.test_env_dir, include_cwd=False), previously_merged=[])
         self.assertEqual('// This is file 1\n// This is file 2\n// This is file 3\n// This is file 4\n', actual_merged_content)
-        clean_up_test_files(paths_to_test_files)
+        helpers.clean_up_test_files(paths_to_test_files)
 
     def test_find_all_in_environment(self):
         paths_to_processable_test_files = [
@@ -271,8 +274,8 @@ class TestResource(unittest.TestCase):
             os.path.join(self.test_env_dir, 'dir1', 'movie.avi'),
             os.path.join(self.test_env_dir, 'some.other.thing')]
 
-        create_test_files(paths_to_processable_test_files)
-        create_test_files(paths_to_unprocessable_test_files)
+        helpers.create_test_files(paths_to_processable_test_files)
+        helpers.create_test_files(paths_to_unprocessable_test_files)
 
         resources = Resource.find_all_in_environment(Environment(self.test_env_dir, include_cwd=False))
 
