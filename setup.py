b51bd25f84b64df928e60ec8a76fea7586190852
Justin Walgran <jwalgran@azavea.com>
2012-02-10 12:11:18 -0500

    Added a setup.py script to the package
    I modeled this script after David Zwarg's python-sld setup.py
    
        https://github.com/azavea/python-sld/blob/master/setup.py

---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+import subprocess
+from distutils.core import setup, Command
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+class RunTests(Command):
+    description = "Run the unit test suite for pipeline."
+    user_options = []
+    extra_env = {}
+    extra_args = []
+
+    def run(self):
+        run_tests_script_path = os.path.join(os.path.dirname(__file__), 'pipeline', 'run_tests.py')
+        subprocess.call([run_tests_script_path])
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+setup(
+    name='Pipeline',
+    version='0.0.1',
+    author='Justin Walgran',
+    author_email='jwalgran@azavea.com',
+    packages=['pipeline', 'pipeline.test'],
+    scripts=['bin/pipeline'],
+    url='http://github.com/azavea/pipeline',
+    license='LICENSE.txt',
+    description='A cross-platform tool for merging and processing client-side assets for a web application.',
+    long_description=read('README.txt'),
+    cmdclass = { 'test': RunTests },
+    keywords = "javascript css html build",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Build Tools"
+    ]
+)
