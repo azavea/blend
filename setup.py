d4ec54e55c90f310bbad4adc2f4bbe1d4e46d1c3
Justin Walgran <jwalgran@azavea.com>
2013-02-05 11:44:02 -0500

    Update version to 0.1.3

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ class RunTests(Command):
 
 setup(
     name='Blend',
-    version='0.1.2',
+    version='0.1.3',
     author='Justin Walgran',
     author_email='jwalgran@azavea.com',
     packages=['blend', 'blend.test'],
